c943c8f68232819af73aa36ab348e092180a09cb
yuan <2442752231@qq.com>
2020-04-26 13:15:31 +0800

    jmeter测试文件和sql文件

---
+++ src/main/resources/miaosha_db.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v11.13 (64 bit)
+MySQL - 5.7.28 : Database - miaosha_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `miaosha_db`;
+
+/*Table structure for table `t_good` */
+
+DROP TABLE IF EXISTS `t_good`;
+
+CREATE TABLE `t_good` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `good_name` varchar(20) CHARACTER SET utf8 DEFAULT NULL COMMENT '商品名称',
+  `stock` int(11) DEFAULT NULL COMMENT '库存',
+  `version` int(11) DEFAULT NULL COMMENT '版本',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='商品';
+
+/*Table structure for table `t_order` */
+
+DROP TABLE IF EXISTS `t_order`;
+
+CREATE TABLE `t_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `order_number` varchar(64) DEFAULT NULL COMMENT '订单编号',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
+  `good_id` bigint(20) DEFAULT NULL COMMENT '商品id',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3298 DEFAULT CHARSET=latin1 COMMENT='订单';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
